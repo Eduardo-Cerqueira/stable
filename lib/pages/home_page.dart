3d56ce391e35b58436731c528aed8b6d01140d01
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-10-17 17:38:03 +0200

    :truck: Change lib directory structure

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:stable/components/home_body.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Stable"),
+          centerTitle: true,
+        ),
+        body: HomeBody(counter: _counter),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      );
+}
