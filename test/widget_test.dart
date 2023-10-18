e0adee6f6e31c2252bf191d2ab35c58403b755dd
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-10-18 10:49:51 +0200

    :white_check_mark: Update test to getX

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:stable/main.dart';
+import 'package:get/get.dart';
+import 'package:stable/pages/home_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GetMaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
