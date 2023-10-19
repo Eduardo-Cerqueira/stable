d01d9f78c9b8f0a3c09a4e6224b024066d6a1342
Coralie Pereira <coralie-p@outlook.fr>
2023-10-19 22:54:08 +0200

    ajout de la photo + photo  en BDD

--- lib/database.dart
+++ lib/database.dart
@@ -2,16 +2,16 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 connection() async {
   const connectionString =
-      'mongodb+srv://coraliepereira95:coraliepereira95@cluster1.fs7xzjy.mongodb.net/Cluster1?retryWrites=true&w=majority';
+      'mongodb+srv://coraliepereira95:coraliepereira95@Cluster1.fs7xzjy.mongodb.net/Cluster1?retryWrites=true&w=majority';
 
-   var db = await Db.create(connectionString);
+  var db = await Db.create(connectionString);
 
   // var db = Db.pool([
   //     connectionString,
   // ]);
   // await db.open();
 
-  GridFS bucket = GridFS(db,"image");
+  // GridFS bucket = GridFS(db,"image");
 
   await db.open(secure: true);
 
@@ -21,11 +21,15 @@ connection() async {
 }
 
 main() async {
-  var db = await connection();
-
+  try {
+    var db = await connection();
+    insertUsers(db, {"name": "keke", "age": 23});
+    print("inserted");
+  } catch (e) {
+    print(e);
+  }
   // getUsers(db);
   // getUsersByName(db , "wanwan");
-  insertUsers(db, {"name": "coralie", "age": 23});
 }
 
 getUsers(db) async {
@@ -41,7 +45,6 @@ getUsers(db) async {
 getUsersByName(db, name) async {
   var collection = db.collection('users');
 
-
   var users = await collection.find(where.eq('name', name)).toList();
 
   print(users);
@@ -56,4 +59,3 @@ Future<void> insertUsers(db, Map<String, dynamic> usersData) async {
 
   await db.close();
 }
-
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:stable/pages/home_page.dart';
 import '../database.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:mongo_dart/mongo_dart.dart'
+    show GridFS; // Importation de la classe Db et GridFS
 
 class MyForm extends StatefulWidget {
   @override
@@ -9,17 +13,25 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  File? _selectedImage;
+
   final _formKey = GlobalKey<FormState>();
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _mailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  TextEditingController _photoController = TextEditingController();
-var _db ; 
-@override
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  var _db;
+  GridFS? bucket;
+  var flag = false;
+  late ImageProvider provider;
+  var _image;
+
+  @override
   void initState() {
     super.initState();
     // Appel de la fonction connection pour obtenir la connexion à la base de données
     connection().then((db) {
+      bucket = GridFS(db, "image");
       setState(() {
         _db = db;
       });
@@ -27,14 +39,60 @@ var _db ;
   }
 
   void _submit() async {
-    if(_db != null){
+    if (_db != null) {
       final String name = _nameController.text;
       final String mail = _mailController.text;
       final String password = _passwordController.text;
-      // var data = {"name": name, "mail" : mail , "password" : password};
-      insertUsers(_db , {"name": name, "mail" : mail , "password" : password}); 
-      print("monsieurWan");
+      insertUsers(_db, {"name": name, "mail": mail, "password": password, "image": _image});
+    } else {
+      print("noDB");
     }
+  }
+
+  Future pickImageFromGallery() async {
+    try {
+      final returnedImage =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if (returnedImage != null) {
+        var _cmpressed_image;
+        try {
+          _cmpressed_image = await FlutterImageCompress.compressWithFile(
+              returnedImage.path,
+              format: CompressFormat.heic,
+              quality: 70);
+        } catch (e) {
+          _cmpressed_image = await FlutterImageCompress.compressWithFile(
+              returnedImage.path,
+              format: CompressFormat.jpeg,
+              quality: 70);
+        }
+        Map<String, dynamic> image = {
+          "_id": returnedImage.path.split("/").last,
+          "data": base64Encode(_cmpressed_image)
+        };
+
+        await bucket?.chunks.insert(image);
+        var img = await bucket?.chunks
+            .findOne({"_id": returnedImage.path.split("/").last});
+        setState(() {
+          _image = returnedImage.path.split("/").last;
+          _selectedImage = File(returnedImage.path);
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future pickImageFromCamera() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if (returnedImage == null) return;
+    setState(() {
+      _selectedImage = File(returnedImage.path);
+    });
   }
 
   @override
@@ -57,9 +115,7 @@ var _db ;
                     return null;
                   },
                 ),
-
-
-                   TextFormField(
+                TextFormField(
                   controller: _mailController,
                   decoration: InputDecoration(labelText: "Adresse mail"),
                   validator: (value) {
@@ -69,9 +125,7 @@ var _db ;
                     return null;
                   },
                 ),
-
-
-                   TextFormField(
+                TextFormField(
                   controller: _passwordController,
                   decoration: InputDecoration(labelText: "Mot de passe"),
                   validator: (value) {
@@ -81,31 +135,31 @@ var _db ;
                     return null;
                   },
                 ),
-
-                
-
-                   TextFormField(
-                  controller: _photoController,
-                  decoration: InputDecoration(labelText: "Photo de profile"),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Veuillez entrer votre photo.';
-                    }
-                    return null;
-                  },
-                ),
-
-
-
-
-
-                SizedBox(height: 20),
+                MaterialButton(
+                    color: Colors.blue,
+                    child: const Text("Pick Image from Gallery",
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold)),
+                    onPressed: () {
+                      pickImageFromGallery();
+                    }),
+                MaterialButton(
+                    color: Colors.blue,
+                    child: const Text("Pick Image from Camera",
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold)),
+                    onPressed: () {
+                      pickImageFromCamera();
+                    }),
+                _selectedImage != null
+                    ? Image.file(_selectedImage!,
+                        width: 150, height: 150, fit: BoxFit.cover)
+                    : const Text("veuillez selectionner une image"),
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      // Le formulaire est valide, vous pouvez traiter les données ici
-                      String name = _nameController.text;
-                      // Faites quelque chose avec le nom, par exemple, l'imprimer
                       _submit();
                     }
                   },
